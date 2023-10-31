fc0faf57cd758b31f05e60ac52921f2bce4322e8
Zimmoonnn <118258618+Buehlsim@users.noreply.github.com>
2023-10-31 10:26:31 +0100

    Update object_catalouge.js (apple Bspl.)

--- generic_website/js/object_catalouge.js
+++ generic_website/js/object_catalouge.js
@@ -3,14 +3,10 @@
  * It contains the objects array with its attributes: name, id, short description, category and long description.
  */
 
-const objects = [
-    { name: "Object", id: 1, short_description: "This is object 1.", category: 1, long_description: "This is a long description of object 1."},
-    { name: "Object", id: 2, short_description: "This is object 2.", category: 2, long_description: "This is a long description of object 2."},
-    { name: "Object", id: 3, short_description: "This is object 3.", category: 1, long_description: "This is a long description of object 3."},
-    { name: "Object", id: 4, short_description: "This is object 4.", category: 2, long_description: "This is a long description of object 4."},
-    { name: "Object", id: 5, short_description: "This is object 5.", category: 1, long_description: "This is a long description of object 5."},
-    { name: "Object", id: 6, short_description: "This is object 6.", category: 2, long_description: "This is a long description of object 6."},
-    { name: "Object", id: 7, short_description: "This is object 7.", category: 1, long_description: "This is a long description of object 7."},
-    { name: "Object", id: 8, short_description: "This is object 8.", category: 2, long_description: "This is a long description of object 8."},
-    { name: "Object", id: 9, short_description: "This is object 9.", category: 1, long_description: "This is a long description of object 9."},
-  ];
+// Daten für die Suchergebnisse (Beispiel)
+export const data = [
+    { title: "iPhone 13", description: "Das neueste iPhone-Modell.", image:"assets/iphone.png" },
+    { title: "MacBook Air", description: "Ein leichtes und leistungsstarkes Notebook.", image:"assets/macbook.png" },
+    { title: "Apple Watch Series 7", description: "Die neueste Smartwatch von Apple.", image:"assets/watch.png" },
+    // Weitere Suchergebnisse hier
+];
