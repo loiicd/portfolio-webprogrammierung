a9f10dd9e4f1860f2b9ac4780f5888a02b5e24c3
Loïc Dörr <loic.debeuf@gmail.com>
2023-11-07 12:44:16 +0100

    add new search

--- generic_website/js/object_catalouge.js
+++ generic_website/js/object_catalouge.js
@@ -11,4 +11,22 @@ const data = [
     // Weitere Suchergebnisse hier
 ];
 
+export const dineData = [
+    { title: "Pizza Salami", description: "Eine köstliche Pizza mit Salami und Käse.", category: "Pizza", image: "../assets/Pizza.png", vegetarisch: false },
+    { title: "Pizza Margherita", description: "Eine klassische Pizza mit Tomaten, Mozzarella und Basilikum.", category: "Pizza", image: "../assets/Pizza.png", vegetarisch: true },
+    { title: "Pizza Funghi", description: "Pizza mit Pilzen, Olivenöl und frischem Knoblauch.", category: "Pizza", image: "../assets/Pizza.png", vegetarisch: true },
+    { title: "Spaghetti Carbonara", description: "Spaghetti mit einer cremigen Eiersauce, Speck und Parmesankäse.", category: "Pasta", image: "../assets/Pasta.png", vegetarisch: false },
+    { title: "Lasagne", description: "Schichtnudeln mit Hackfleisch, Tomatensauce und Käse überbacken.", category: "Pasta", image: "../assets/Pasta.png", vegetarisch: false },
+    { title: "Ravioli mit Pesto", description: "Ravioli gefüllt mit Ricotta und Spinat, serviert mit Pesto.", category: "Pasta", image: "../assets/Pasta.png", vegetarisch: true },
+    { title: "Hamburger", description: "Ein saftiger Hamburger mit Rindfleisch, Tomaten, Salat und Käse.", category: "Burger", image: "../assets/Burger.png", vegetarisch: false },
+    { title: "Chicken Burger", description: "Hamburger mit knusprig gebratenem Hühnchen, Salat und Mayonnaise.", category: "Burger", image: "../assets/Burger.png", vegetarisch: false },
+    { title: "Veggie Burger", description: "Ein vegetarischer Burger mit Gemüsepatty und Avocado.", category: "Burger", image: "../assets/Burger.png", vegetarisch: true },
+    { title: "Sushi Combo", description: "Eine Auswahl an frischem Sushi mit verschiedenen Sorten und Sojasauce.", category: "Sushi", image: "../assets/DS_Logo.png", vegetarisch: false },
+    { title: "Sashimi Deluxe", description: "Frische Sashimi-Scheiben von Thunfisch und Lachs.", category: "Sushi", image: "../assets/DS_Logo.png", vegetarisch: false },
+    { title: "Tempura Roll", description: "Sushi-Rolle mit knusprig gebackenem Tempura und Avocado.", category: "Sushi", image: "../assets/DS_Logo.png", vegetarisch: false },
+    { title: "Caesar Salad", description: "Ein knackiger Caesar-Salat mit Hühnchen, Croutons und Caesar-Dressing.", category: "Salat", image: "../assets/DS_Logo.png", vegetarisch: false },
+    { title: "Greek Salad", description: "Griechischer Salat mit Tomaten, Gurken, Oliven und Feta-Käse.", category: "Salat", image: "../assets/DS_Logo.png", vegetarisch: true },
+    { title: "Cobb Salad", description: "Cobb-Salat mit Hühnchen, Bacon, Eiern und Avocado.", category: "Salat", image: "../assets/DS_Logo.png", vegetarisch: false }
+];
+
 export default data
