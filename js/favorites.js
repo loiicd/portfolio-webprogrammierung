765b065e7ea189261ad59c730f782387fd599ea4
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-02 00:01:57 +0100

    clean up

--- js/favorites.js
+++ js/favorites.js
@@ -1,11 +1,10 @@
 import { ObjectContainer } from './components/ObjectTile.js';
 
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-if (favorites.length === 0) {
-  container.container.innerHTML = 'Keine Favoriten vorhanden.';
-}
 
 const container = new ObjectContainer('objectDisplay-Container');
 container.render(favorites);
 
-window.addEventListener('favoritesUpdated', () => {container.render(favorites);});
+window.addEventListener('favoritesUpdated', () => {
+  let favorites = JSON.parse(localStorage.getItem('favorites'))  || [];
+  container.render(favorites);}); 
