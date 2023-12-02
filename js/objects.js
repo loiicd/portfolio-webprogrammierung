2c3b3e854c7f9756352778b4dc363e28c1f43157
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-02 09:54:08 +0100

    all objects are displayed initially

--- js/objects.js
+++ js/objects.js
@@ -4,6 +4,7 @@ import { dineData } from './components/objectData.js';
 const searchInput = document.getElementById('search-input');
 
 const container = new ObjectContainer('objectDisplay-Container');
+container.render(dineData);
 
 searchInput.addEventListener('input', () => {
   const searchText = searchInput.value;
