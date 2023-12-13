895c4a1e1a4e6314674e5c23df67cd84c5801bd7
Loïc Dörr <loic.debeuf@gmail.com>
2023-12-13 21:55:24 +0100

    edits

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@ import { ObjectContainer } from './components/ObjectTile.js';
 import { dineData } from './components/objectData.js';
 import { clearCompletedOrders } from './components/updateLocalStorage.js';
 
-const recomendedatoins = dineData.slice(0,3); // implement getRecommendations() function in ./components/recomendations.js
+const getRecommendations = (dineData) => {
+  const shuffled = dineData.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, 4);
+}
+
+const recomendedatoins = getRecommendations(dineData)
 
 const container = new ObjectContainer('objectDisplay-Container');
 container.render(recomendedatoins);
