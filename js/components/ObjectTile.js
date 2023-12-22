58f7729f3968ad7c355991a8f15d426aa2e8fa90
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-22 13:45:47 +0100

    Document and refactor ObjectTile and ObjectContainer classes

--- js/components/ObjectTile.js
+++ js/components/ObjectTile.js
@@ -4,17 +4,19 @@
 
 import { isInLocalStorage, removeFromLocalStorage, addToLocalStorage } from "./updateLocalStorage.js";
 
-
 /**
- * @description Class which represents a container of ObjectTiles.
- * @constructor id of html-div to display the container in.
- * @function render() takes a array of Objects which will be displayed.
+ * Class representing a Container for object tiles.
+ * @property {HTMLElement} container - The HTML element for the container specified by the given id.
  */
 class ObjectContainer {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
   }
 
+  /**
+   * Render the given objects in the container.
+   * @param {Array} objects - The objects to render.
+   */
   render(objects) {
     this.container.innerHTML = '';
     objects.forEach(object => {
@@ -24,77 +26,129 @@ class ObjectContainer {
   }
 }
 
+/**
+ * Class representing a tile for an object.
+ * @property {Object} object - The object to display.
+ */
 class ObjectTile {
   constructor(object) {
     this.object = object;
     this.element = this.createElement();
   }
 
+  /**
+   * Render the tile in the given container.
+   * @param {HTMLElement} container - The HTML element for the container.
+  */
+  render(container) {
+    container.append(this.element);
+  }
+
+  /**
+   * Create the HTML element for the tile with all its children: 
+   * tile header with image, tile center button, tile title, tile description
+   * @return {HTMLElement} The created element.
+   */
   createElement() {
     const objectTile = document.createElement('div');
-    objectTile.classList.add('object-tile');
-    objectTile.classList.add('card');
-    objectTile.classList.add('hover');
+    objectTile.classList.add('object-tile', 'card', 'hover');
 
-    const tileHeader = document.createElement('div');
-    tileHeader.classList.add('tile-header');
-
-    const image = document.createElement('img');
-    image.src = this.object.image;
-    image.alt = this.object.title;
-
-    const updateFavouritesIcon = document.createElement('img');
-    updateFavouritesIcon.classList.add('favourites-icon');
-    updateFavouritesIcon.addEventListener('click', () => this.updateFavorites());
-    this.updateFavoritesDisplay(updateFavouritesIcon);
-
-    tileHeader.append(image);
-    
-    const favouritesDiv = document.createElement('div');
-    favouritesDiv.classList.add('favourites-div');
-    favouritesDiv.append(updateFavouritesIcon);
-    
-    tileHeader.append(favouritesDiv);
-
-    const tileCenterButtons = document.createElement('div');
-    tileCenterButtons.classList.add('tile-center-buttons');
-    
-    const updateOrderButton = document.createElement('button');
-    updateOrderButton.addEventListener('click', () => this.updateOrder());
-    this.updateOrderDisplay(updateOrderButton);
-    tileCenterButtons.append(updateOrderButton);
-
-    const tileTitle = document.createElement('div');
-    tileTitle.classList.add('tile-title');
-
-    const title = document.createElement('h3');
-    title.textContent = this.object.title;
-
-    tileTitle.append(title);
-
-    const tileDescription = document.createElement('div');
-    tileDescription.classList.add('tile-description');
-
-    const expandButton = document.createElement('a');
-    expandButton.textContent = 'Description ...';
-    expandButton.classList.add('tile-description-button');
-    expandButton.href = '##';
-    expandButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.toggleExpand(objectTile, expandButton);
-    });
-
-    const expandedText = document.createElement('p');
-    expandedText.textContent = this.object.description.length > 70 ? this.object.description.substring(0, 67) + '...' : this.object.description;
-    expandedText.style.display = 'none';
-
-    tileDescription.append(expandButton, expandedText);
+    const tileHeader = this.createTileHeader();
+    const tileCenterButtons = this.createTileCenterButtons();
+    const tileTitle = this.createTileTitle();
+    const tileDescription = this.createTileDescription();
 
     objectTile.append(tileHeader, tileCenterButtons, tileTitle, tileDescription);
 
     return objectTile;
   }
 
+  createTileHeader() {
+    const tileHeader = document.createElement('div');
+    tileHeader.classList.add('tile-header');
+    const image = this.createImage();
+    const updateFavouritesIcon = this.createIcon('favourites-icon', () => this.updateFavorites());
+    this.updateFavoritesDisplay(updateFavouritesIcon);
+    tileHeader.append(image, this.createDiv('favourites-div', updateFavouritesIcon));
+    return tileHeader;
+  }
+
+  createImage() {
+    const image = document.createElement('img');
+    image.src = this.object.image;
+    image.alt = this.object.title;
+    return image;
+  }
+
+  createTileCenterButtons() {
+    const tileCenterButtons = this.createDiv('tile-center-buttons');
+    const updateOrderButton = this.createButton(() => this.updateOrder());
+    this.updateOrderDisplay(updateOrderButton);
+    tileCenterButtons.append(updateOrderButton);
+    return tileCenterButtons;
+  }
+
+  createTileTitle() {
+    const tileTitle = this.createDiv('tile-title');
+    const title = document.createElement('h3');
+    title.textContent = this.object.title;
+    tileTitle.append(title);
+    return tileTitle;
+  }
+
+  createTileDescription() {
+    const tileDescription = this.createDiv('tile-description');
+    const expandButton = this.createLink('tile-description-button', 'Description ...', '##', (event) => {
+      event.preventDefault();
+      this.toggleExpand(objectTile, expandButton);
+    });
+    const expandedText = this.createExpandedText();
+    tileDescription.append(expandButton, expandedText);
+    return tileDescription;
+  }
+
+  createExpandedText() {
+    const expandedText = document.createElement('p');
+    expandedText.textContent = this.object.description.length > 70 ? this.object.description.substring(0, 67) + '...' : this.object.description;
+    expandedText.style.display = 'none';
+    return expandedText;
+  }
+
+  createIcon(className, clickHandler) {
+    const icon = document.createElement('img');
+    icon.classList.add(className);
+    icon.addEventListener('click', clickHandler);
+    return icon;
+  }
+
+  createDiv(className, childElement) {
+    const div = document.createElement('div');
+    div.classList.add(className);
+    if (childElement) {
+      div.append(childElement);
+    }
+    return div;
+  }
+
+  createButton(clickHandler) {
+    const button = document.createElement('button');
+    button.addEventListener('click', clickHandler);
+    return button;
+  }
+
+  createLink(className, text, href, clickHandler) {
+    const link = document.createElement('a');
+    link.textContent = text;
+    link.classList.add(className);
+    link.href = href;
+    link.addEventListener('click', clickHandler);
+    return link;
+  }
+
+  /**
+   * Update the favorites in the local storage.
+   * @fires localStorageUpdated
+   **/
   updateFavorites() {
     if (isInLocalStorage(this.object, "favorites")) {
       removeFromLocalStorage(this.object, "favorites");
@@ -106,13 +160,13 @@ class ObjectTile {
   }
 
   updateFavoritesDisplay(element) {
-    if (isInLocalStorage(this.object, "favorites")) {
-      element.src = '../assets/icons/favicon-true.png';
-    } else {
-      element.src = '../assets/icons/favicon-false.png';
-    }
+    element.src = isInLocalStorage(this.object, "favorites") ? '../assets/icons/favicon-true.png' : '../assets/icons/favicon-false.png';
   }
 
+    /**
+   * Update the orders in the local storage.
+   * @fires localStorageUpdated
+   **/
   updateOrder() {
     if (isInLocalStorage(this.object, "orders")) {
       removeFromLocalStorage(this.object, "orders");
@@ -125,34 +179,19 @@ class ObjectTile {
   }
 
   updateOrderDisplay(element) {
-    if (isInLocalStorage(this.object, "orders")) {
-      element.classList.add('remove-from-orders');
-      element.classList.remove('add-to-orders');
-      if (element.tagName === 'BUTTON') {
-        element.textContent = 'Remove from Order';
-      }
-    } else {
-      element.classList.add('add-to-orders');
-      element.classList.remove('remove-from-orders');
-      if (element.tagName === 'BUTTON') {
-        element.textContent = 'Add to Order';
-      }
+    const isObjectInOrders = isInLocalStorage(this.object, "orders");
+    element.classList.toggle('remove-from-orders', isObjectInOrders);
+    element.classList.toggle('add-to-orders', !isObjectInOrders);
+    if (element.tagName === 'BUTTON') {
+      element.textContent = isObjectInOrders ? 'Remove from Order' : 'Add to Order';
     }
   }
 
   toggleExpand(objectTile, expandButton) {
     const paragraph = objectTile.querySelector('p');
-    if (paragraph.style.display === "none") {
-      paragraph.style.display = "block";
-      expandButton.textContent = 'Hide';
-    } else {
-      paragraph.style.display = "none";
-      expandButton.textContent = 'Description ...';
-    }
-  }
-
-  render(container) {
-    container.append(this.element);
+    const isParagraphHidden = paragraph.style.display === "none";
+    paragraph.style.display = isParagraphHidden ? "block" : "none";
+    expandButton.textContent = isParagraphHidden ? 'Hide' : 'Description ...';
   }
 }
 
