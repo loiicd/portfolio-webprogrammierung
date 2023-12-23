eb1bd8f26d251237e7cedacae6896094e95a08ba
Loïc Dörr <loic.debeuf@gmail.com>
2023-12-23 16:52:40 +0100

    edits

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { ObjectContainer } from './components/ObjectTile.js';
 import dineData from './data/objectData.js';
-import { clearLocalStorage } from './components/updateLocalStorage.js';
+import { clearLocalStorage, getLocalStorage } from './components/updateLocalStorage.js';
 import getRecommendations from './components/getRecomendations.js';
 
 
@@ -10,6 +10,8 @@ import getRecommendations from './components/getRecomendations.js';
 const recomendedatoins = getRecommendations(dineData);
 const container = new ObjectContainer('objectDisplay-Container');
 const deleteHistoryButton = document.getElementById('deleteHistoryButton');
+// const showOrders = document.getElementById('show-orders');
+// const completedOrders = getLocalStorage('completedOrders');
 
 
 //- * * * * * * * * * * * *
